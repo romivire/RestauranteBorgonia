2491f473e6e7f8b07b5718ba93eadd9277106d12
LAPTOP-V68SKF4G\romiv <romivire@gmail.com>
2021-04-28 14:14:13 -0300

    Add routes and middleware by route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::middleware(['auth'])->group(function () {
+    Route::get('/platos','App\Http\Controllers\PlatoController@index')->name('platos.index');
+    Route::get('/platos/create','App\Http\Controllers\PlatoController@create')->name('platos.create');
+    Route::get('/platos/{id}/edit','App\Http\Controllers\PlatoController@edit')->name('platos.edit');
+    Route::patch('/platos/{id}','App\Http\Controllers\PlatoController@update')->name('platos.update');
+    Route::post('/platos','App\Http\Controllers\PlatoController@store')->name('platos.store');
+    Route::delete('/platos/{id}','App\Http\Controllers\PlatoController@destroy')->name('platos.destroy');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/reservas','App\Http\Controllers\ReservaController@index')->name('reservas.index');
+    Route::get('/reservas/create','App\Http\Controllers\ReservaController@create')->name('reservas.create');
+    Route::get('/reservas/{id}/edit','App\Http\Controllers\ReservaController@edit')->name('reservas.edit');
+    Route::patch('/reservas/{id}','App\Http\Controllers\ReservaController@update')->name('reservas.update');
+    Route::post('/reservas','App\Http\Controllers\ReservaController@store')->name('reservas.store');
+    Route::delete('/reservas/{id}','App\Http\Controllers\ReservaController@destroy')->name('reservas.destroy');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/restaurantes','App\Http\Controllers\RestauranteController@index')->name('restaurantes.index');
+    Route::get('/restaurantes/{id}/edit','App\Http\Controllers\RestauranteController@edit')->name('restaurantes.edit');
+    Route::patch('/restaurantes/{id}','App\Http\Controllers\RestauranteController@update')->name('restaurantes.update');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
