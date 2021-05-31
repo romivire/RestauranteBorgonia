40df4843e16c1ae5ccf89d02d6ee44b7a8f51b26
LAPTOP-V68SKF4G\romiv <romivire@gmail.com>
2021-05-31 15:44:36 -0300

    update seeder Plato. Todos los platos del seeder se inicializan sin imagen

--- database/seeders/PlatosSeeder.php
+++ database/seeders/PlatosSeeder.php
@@ -18,56 +18,50 @@ class PlatosSeeder extends Seeder
             'nombre' => 'Ensalada Cesar',
             'precio' => 3,
             'categoria_plato' => 'Ensalada',
-            'vegetariano' => 'Si',
-            'imagen' => 'plato2.jpg'
+            'vegetariano' => 'Si'
+            
             
         ]);
         DB::table('platos')->insert([
             'nombre' => 'Goulash con Spaetzle',
             'precio' => 50,
             'categoria_plato' => 'Carne',
-            'vegetariano' => 'No',
-            'imagen' => 'plato5.jpg'
+            'vegetariano' => 'No'
             
         ]);
         DB::table('platos')->insert([
             'nombre' => 'Panqueques con dulce de leche',
             'precio' => 50,
             'categoria_plato' => 'Postre',
-            'vegetariano' => 'Si',
-            'imagen' => 'plato6.jpg'
+            'vegetariano' => 'Si'
             
         ]);
         DB::table('platos')->insert([
             'nombre' => 'Milanesa napolitana',
             'precio' => 70,
             'categoria_plato' => 'Carne',
-            'vegetariano' => 'No',
-            'imagen' => 'plato4.jpg'
+            'vegetariano' => 'No'
             
         ]);
         DB::table('platos')->insert([
             'nombre' => 'Cazuela de mariscos',
             'precio' => 100,
             'categoria_plato' => 'Pescado',
-            'vegetariano' => 'Si',
-            'imagen' => 'plato3.jpg'
+            'vegetariano' => 'Si'
             
         ]);
         DB::table('platos')->insert([
             'nombre' => 'Bruschettas',
             'precio' => 65,
             'categoria_plato' => 'Ensalada',
-            'vegetariano' => 'Si',
-            'imagen' => 'plato1.jpg'
+            'vegetariano' => 'Si'
             
         ]);
         DB::table('platos')->insert([
             'nombre' => 'Ravioles con salsa bolognesa',
             'precio' => 50,
             'categoria_plato' => 'Pasta',
-            'vegetariano' => 'No',
-            'imagen' => 'plato7.jpg'
+            'vegetariano' => 'No'
             
         ]);
     }
